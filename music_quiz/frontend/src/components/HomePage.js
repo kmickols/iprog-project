8fc93b45d9a95c54880b99bd95e8da79e56e45ec
unknown <>
2021-03-29 09:57:21 +0200

    added filex + fix warnings when running npm

--- music_quiz/frontend/src/components/HomePage.js
+++ music_quiz/frontend/src/components/HomePage.js
@@ -1,15 +1,11 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
-import RoomJoinPage from "./RoomJoinPage"
-import CreateRoomPage from "./CreateRoomPage";
 
 export default function HomePage(props){
     return (
         <Router>
             <switch>
                 <Route exact path='/'><p> This is the homepage! </p> </Route>
-                <Route path='/join' component={RoomJoinPage}/>
-                <Route path='/create' component={CreateRoomPage}/>
             </switch>
         </Router>
     );
