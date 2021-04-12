0448ac76aa4ba2ad743b842032f0f158b1537685
Kristin Mickols <kmickols@kth.se>
2021-04-12 22:36:46 +0200

    Styling w CSS + Views

---
+++ music_quiz/frontend/src/views/inputQuestion.js
@@ -0,0 +1,30 @@
+function InputQuestion(props) {
+    return(
+        <div>
+        <header>
+            <h1 class="header-side"> Music Quiz</h1>
+        </header>
+
+    <span class="main-text">
+
+      <!--Question goes here-->
+
+      <table style="display: grid; justify-content: center">
+       <tr>
+        <td>
+              <input placeholder="Type answer text here" class="fill-form"/>
+        </td>
+
+      </tr>
+      <tr>
+        <td>
+<input placeholder="Type answer text here" class="fill-form"/>
+        </td>
+      </tr>
+      </table>
+    </span>
+
+</div>
+    )
+
+}
---
+++ music_quiz/frontend/src/views/startView.js
@@ -0,0 +1,13 @@
+function StartView() {
+    return(
+        <div>
+        <header>
+            <h1 class="header"> Music Quiz</h1>
+        </header>
+    <span class = "main-text">
+      <button class="start-button">Host a quiz</button>
+      <button class="start-button">Join a quiz</button>
+      <button class="start-button">Tutorial</button>
+      </span>
+        </div>
+    );}
