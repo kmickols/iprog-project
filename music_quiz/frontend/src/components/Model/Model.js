a2ef09328849408ef71bc2dd17d4d91955a00576
Anton Björklund <antbjor@gmail.com>
2021-04-13 16:18:47 +0200

    inital Model work

---
+++ music_quiz/frontend/src/components/Model/Model.js
@@ -0,0 +1,34 @@
+class Model {
+    constructor(players = 2, questions = [], currentQ = 0, numberOfQs = 1, roomCode = "") {
+        this.questions = dishes;
+        this.currentQ = currentQ;
+        this.roomCode = roomCode;
+        this.setNumberOfPlayers(players);
+        this.setNumberOfQs(numberOfQs);
+        this.getNextQ(currentQ);
+    }
+
+    setNumberOfPlayers(x){
+        this.players = x;
+    }
+
+    setNumberOfQs(x) {
+        this.numberOfQs = x;
+    }
+
+    getNextQ(){
+        if((currentQ +1 ) != numberofQs){ // We're not on the last Q
+            this.currentQ = currentQ +1;
+            this.getQuestion(currentQ);
+        }/* else{
+            //GEJM OVER
+        }*/
+    }
+    getQuestion(x){
+        let questionElement = this.questions[x];
+        return questionElement;
+    }
+
+    // updateScore
+    
+}
