d24c8fa155631095fc70ade5fbc9199ae553eb30
Kristin Mickols <kmickols@kth.se>
2021-04-15 14:24:40 +0200

    Bug fix with buttons

--- music_quiz/frontend/src/views/startView.js
+++ music_quiz/frontend/src/views/startView.js
@@ -5,9 +5,9 @@ export default function StartView({hostGame, joinGame, tutorial}) {
         <div>
 
             <span className= "main-text">
-              <button className="button start-button" onClick={() => hostGame()}>Host a quiz</button>
-              <button className="button start-button" onClick={() => joinGame()}>Join a quiz</button>
-              <button className="button start-button" onClick={() => tutorial()}>Tutorial</button>
+              <button className="button" onClick={() => hostGame()}>Host a quiz</button>
+              <button className="button" onClick={() => joinGame()}>Join a quiz</button>
+              <button className="button" onClick={() => tutorial()}>Tutorial</button>
             </span>
         </div>
     );}
