94872f2eaf25b4fd7b5772b8ab92cb180fe06ae8
Kristin Mickols <kmickols@kth.se>
2021-04-15 13:54:37 +0200

    fixade styling

--- music_quiz/frontend/src/views/joinRoom.js
+++ music_quiz/frontend/src/views/joinRoom.js
@@ -6,14 +6,22 @@ export default function JoinRoom({joinGame}) {
             <span className="main-text">
                 Room Code
             </span>
+        <span className="main-text">
             <br/>
             <input id="code" placeholder="Room Code" maxLength={6} className="fill-form"/>
+        </span>
             <br/>
+        <span className="main-text">
+
             Nickname
+        </span>
+        <span className="main-text"> 
             <br/>
             <input id="name" placeholder="Nickname" maxLength={15} className="fill-form"/>
             <br/>
-            <button className="button launch-button" onClick={() => {() => joinGame()}}>
-                Join Room!</button>
+            </span>
+            <span className="main-text">     <button className="button launch-button" onClick={() => {() => joinGame()}}>
+                Join Room!</button></span>
+
         </div>)
 }
