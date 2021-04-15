2bab26f681b1c4f774e0126c35c7d5e1cd7e294a
Kristin Mickols <kmickols@kth.se>
2021-04-15 17:19:28 +0200

    git fix

---
+++ music_quiz/frontend/src/views/createRoom.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import {createRoom} from "../components/roomAPI";
+
+// Logga in med Spotify
+// Välj spellista (Richard)
+// Hur många frågor
+// (ev. Vilken typ av frågor)
+export default function CreateRoom({spotifyUsername, loggedInToSpotify, numQuestions, changeNumQuestions, loginSpotify, createRoom, returnToMain}) {
+    return(
+        <div>
+            <div style={{margin: 30}}>
+                <span className={"main-text"}>
+                    <button className="button smaller-button">Login with Spotify!</button>
+                </span>
+            </div>
+            <div style={{margin: 30}}>
+                <span className="main-text">
+                Choose Playlist:
+                </span>
+            </div>
+            <div style={{marginTop: 30}}>
+                <span className="main-text">
+                   Number of Questions:
+                    <span className={"main-text"}>
+                        <button className="button mini-button" onClick={() => changeNumQuestions(numQuestions + 1)}>+</button>
+                        <span className="main-text">{numQuestions}</span>
+                        <button className="button mini-button" onClick={() => changeNumQuestions(numQuestions - 1)}>-</button>
+
+                    </span>
+                </span>
+            </div>
+            <span className={"main-text"}>
+                <button className="button smaller-button" onClick={() => createRoom()}>Create Room</button>
+            </span>
+            <span className={"main-text"}>
+                <button className="button smaller-button" onClick={() => returnToMain()}> Back </button>
+            </span>
+        </div>
+    );
+}
