06ede3b25836b2f70a56f8d810a8bef8dce302f9
unknown <>
2021-04-16 15:24:06 +0200

    deploy

--- music_quiz/spotify/credentials.py
+++ music_quiz/spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "ffb248a27b0c44caa25b81b8422cc036"
 CLIENT_SECRET = "528d751ba556459eb79816f104d8adee"
-REDIRECT_URI = "https://iprog-project.herokuapp.com/"
+REDIRECT_URI = "https://iprog-project.herokuapp.com/spotify/redirect"
