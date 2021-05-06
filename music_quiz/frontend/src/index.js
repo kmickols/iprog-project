4f0d59760b1da9a3449c77c40695880334f9d9f4
unknown <>
2021-05-06 12:02:24 +0200

    clean up

--- music_quiz/frontend/src/index.js
+++ music_quiz/frontend/src/index.js
@@ -3,4 +3,6 @@ import Model from "./model/model";
 import {render} from "@testing-library/react";
 import React from "react";
 
-
+window.onSpotifyWebPlaybackSDKReady  = () => {
+        //Do nothing
+}
