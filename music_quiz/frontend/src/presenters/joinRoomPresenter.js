8d067a69d7b87ad1affa2a82b544263d9e7d4a85
Richard Lästh <rlasth@kth.se>
2021-05-13 13:59:20 +0200

    model and presenter refractored

--- music_quiz/frontend/src/presenters/joinRoomPresenter.js
+++ music_quiz/frontend/src/presenters/joinRoomPresenter.js
@@ -2,32 +2,33 @@ import React from "react";
 import JoinRoom from "../views/joinRoom";
 import {joinRoom} from "../components/roomAPI";
 
-export default function JoinRoomPresenter(props){
-
+export default function JoinRoomPresenter(props) {
+    const model = props.model
     const [promise, setPromise] = React.useState(null)
     const [data, setData] = React.useState(null)
     const [error, setError] = React.useState(null)
 
-    React.useEffect(function (){
+    React.useEffect(function () {
             setData(null)
             setError(null)
-            if (promise){
-                const p=promise
-                promise.then(dt=>{
-                    if(promise===p){
+            if (promise) {
+                const p = promise
+                promise.then(dt => {
+                    if (promise === p) {
                         setData(dt)
-                        props.history.push("/room/"+dt.code)
-                        props.model.setIsHost(false)
-                        props.model.score = 0
-                        props.model.nickname = dt.user_name
+                        props.history.push("/room/" + dt.code)
+                        model.setIsHost(false)
+                        model.score = 0
+                        model.nickname = dt.user_name
+                        model.setRoomCode(dt.code)
                     }
-                }).catch(er=>{
-                        if(promise===p){
+                }).catch(er => {
+                        if (promise === p) {
                             return er
                         }
                     }
                 ).then(er => {
-                            setError(er);
+                    setError(er);
                 })
             }
 
@@ -42,36 +43,36 @@ export default function JoinRoomPresenter(props){
 
 
     return <JoinRoom joinGame={() => {
-                        let tmp_name = name.trim()
-                        let tmp_code = code.trim().toUpperCase()
-                        let err = false
-                        if(tmp_name.length < 1){
-                            err = true
-                            setNameErr("Enter a name with at least 1 character")
-                        } else {
-                            setNameErr("")
-                        }
+        let tmp_name = name.trim()
+        let tmp_code = code.trim().toUpperCase()
+        let err = false
+        if (tmp_name.length < 1) {
+            err = true
+            setNameErr("Enter a name with at least 1 character")
+        } else {
+            setNameErr("")
+        }
 
-                        if(tmp_code.length < 6){
-                            err = true
-                            setCodeErr("Enter a code that is at least 6 characters")
-                        } else {
-                            setCodeErr("")
-                        }
+        if (tmp_code.length < 6) {
+            err = true
+            setCodeErr("Enter a code that is at least 6 characters")
+        } else {
+            setCodeErr("")
+        }
 
-                        if(!err){
-                            setPromise(joinRoom(tmp_code, tmp_name))
-                        }
+        if (!err) {
+            setPromise(model.getJoinRoom(tmp_code, tmp_name))
+        }
 
-                    }}
+    }}
                      code={code}
                      name={name}
                      codeInput={val => setCode(val)}
                      nameInput={val => setName(val)}
-                     joinErr={error?(error.user_message?error.user_message:error.message):""}
+                     joinErr={error ? (error.user_message ? error.user_message : error.message) : ""}
                      codeError={codeErr}
                      nameError={nameErr}
-                     err={error?error.message:""}
+                     err={error ? error.message : ""}
                      returnToMain={() => props.history.push("/")}
     />
-    }
+}
