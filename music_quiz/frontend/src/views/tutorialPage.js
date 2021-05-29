5632d44cf79742702214bd6c231bd1834e9e7cdd
Kristin Mickols <kmickols@kth.se>
2021-05-29 13:02:09 +0200

    changed tutorial page

--- music_quiz/frontend/src/views/tutorialPage.js
+++ music_quiz/frontend/src/views/tutorialPage.js
@@ -1,45 +1,50 @@
 import React from "react";
 
 export default function TutorialPage({returnToMain}) {
+
     return(
-        <div>
-
+        <div align={"center"}>
         <span className="main-text extra-bold-text"> <p> HOW TO PLAY! </p></span>
-        <span className="main-text">
-        <ul>
-            <li>Hosting a game:</li>
-            <ul>
-                <li>Choose "Host a quiz" in the home screen!</li>
-                <li>Choose a genre to generate music from and a number of questions.</li>
-                <li>Create the room</li>
-                <li>Wait for other people to join the room.</li>
-                <li>When everyone is ready, start the game!</li>
+            <span className="main-text dist">
+                <button type="button" className="collapsible">Host a game</button>
+<div align={"center"} className="content smaller-text" >
+    <ul>
+    <li>Choose "Host a quiz" on the home screen</li>
+    <li>Log in with your Spotify Premium account </li>
+    <li>Choose a music genre and number of questions</li>
+    <li>Choose "Create room" to generate a room code</li>
+    <li>Wait for the players to join the room with the provided room code</li>
+    <li>When everyone is ready, start the game!</li>
+</ul>
+</div>
+            </span>
+            <span className="main-text dist">
+<button type="button" className="collapsible">Join a game</button>
+<div align={"center"} className="content main-text smaller-text">
+    <ul>
+  <li>Choose "Join a quiz" on the home screen</li>
+    <li>Enter the room code provided by the host </li>
+    <li>Enter a cool nickname</li>
+    <li>Wait for the game to begin!</li>
+</ul>
+</div>
+                </span>
+            <span className="main-text dist">
+<button type="button" className="collapsible ">How to play</button>
+<div align={"center"} className="content main-text smaller-text">
+    <ul>
+    <li> The host will play a song from the chosen genre for 30 seconds </li>
+    <li> Your goal is to identify the song and answer the questions about it</li>
+    <li>Enter your answer and submit it before the time runs out!</li>
+    <li>You gain one point for each correct answer</li>
+    <li>The correct answers will be shown when the time runs out</li>
+    <li>Wait for the host to proceed to next question</li>
+    <li>When all questions have been answered, the scores on the leaderboard will be shown!</li>
         </ul>
-            <br/>
-            <li>Joining a game:</li>
-            <ul>
-                <li>Choose "Join a quiz" from the home screen.</li>
-                <li>Enter the "Room code" of the room you wish to join.</li>
-                <li>Enter a nickname. This is the name other players will see.</li>
-                <li>Wait for the game to start!</li>
-            </ul>
-            <br/>
-        <li>How to play:</li>
-            <ul>
-                <li>After the host starts the game, different songs will be played.</li>
-                <li>You will be asked different questions about the song, such as the name of the song.</li>
-                <li>Enter your answer and submit it before the time runs out!</li>
-                <li>You gain one point for each correct answer.</li>
-                <li>After each question is finished the answers will be shown.</li>
-                <li>The host must now proceed to the next question.</li>
-                <li>After all questions the result will be shown!</li>
-            </ul>
-      </ul>
-
-        </span>
-            <span className="main-text"> <button className="button mini-button" onClick={() => returnToMain()}> &#5130; Back
+</div>
+            </span>
+            <span className="main-text dist"> <button className="button mini-button" onClick={() => returnToMain()}> &#5130; Back
         </button> </span>
         </div>
     )
-
 }
